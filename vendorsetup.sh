8df41b635d51ce2488f16046fddef706939d3bba
Sagar Shah <sagarshah2326@gmail.com>
2016-12-28 16:45:13 +0100

    Fix Vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-# Copyright (C) 2016 The Android OpenSource Project
+# Copyright (C) 2016 Cosmic-OS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
