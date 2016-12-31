531d6ed7c7805b51089448fc1be6afb2fb164e6a
maxwen <max.weninger@gmail.com>
2016-12-31 18:37:53 +0100

    vendor: bring back DSPManager effects backend
    it has more features then standard android effects
    
    devices that dont want to use it should simply
    add a copy of standard audio_effects.conf file into the
    device and copy this into /system/etc/audio_effects_vendor.conf
    using that overwrite mechanism
    
    DU edit: Don't want it, don't use it (TARGET_NO_DSPMANAGER := true)
    
    Change-Id: Ibc21efcc3125fb299eb26371c93f7559df159461

--- common.mk
+++ common.mk
@@ -14,7 +14,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
 
-# Thank you, please drive thru!
+# For stereo widening effect
+ifneq ($(TARGET_NO_DSPMANAGER), true)
+ PRODUCT_PACKAGES += \
+ libcyanogen-dsp \
+ audio_effects.conf
+endif
+
+# Thank you, please drive thru! 
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
 
 # Backup Tool
