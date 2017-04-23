21196d0e342be5f0c3bfb144b0631e2197484cf9
William Roberts <wroberts@tresys.com>
2017-04-23 09:02:28 +0200

    Common sepolicy
    Change-Id: I28a4aaf6c126535f0a88001582641b234a750015

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -5,3 +5,11 @@
 
 BOARD_SEPOLICY_DIRS += \
     vendor/cos/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    file_contexts \
+    file.te \
+    genfs_contexts \
+    installd.te \
+    mac_permissions.xml \
+	vold.te
