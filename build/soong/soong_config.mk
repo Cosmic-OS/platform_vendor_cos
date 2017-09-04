9d5380af865f582b1ca37e26d565e3dc5449fbe6
Simon Shields <simon@lineageos.org>
2017-09-04 21:11:16 +1000

    build: soong: put cosmic variables in the "Cosmic" struct
    Change-Id: I9ed55cea0c55f0078f8f97ad4037b16ed11c0156

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,5 +1,8 @@
+# Insert new variables inside the Cosmic structure
 cosmic_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
+	echo '"Cosmic": {'; \
+	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
