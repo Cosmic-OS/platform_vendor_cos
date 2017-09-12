d6eb671e257bb437dac62e4d08350a72479aadc1
Josh Chasky <jchasky@gmail.com>
2017-09-12 16:39:42 -0400

    Update flags for O
    Change-Id: I5667575311718b7f77b4deb02ab990635d804fbb

--- common.mk
+++ common.mk
@@ -5,11 +5,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=android-google \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
-    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.error.receiver.system.apps=com.google.android.gms \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=MM-dd-yyyy \
     ro.com.android.dataroaming=false \
-    ro.opa.eligible_device=true \
+    ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
     ro.setupwizard.rotation_locked=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
