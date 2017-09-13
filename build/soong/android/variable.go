ce72f8c9ec4c21b42d216a79b82d70e26ef6a6ab
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-13 14:55:00 -0400

    vendor/cos: Add qcom board flags to soong
    Change-Id: I21397b5b765e323ec6d5cdab23b93c9b885d7d23

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -15,9 +15,28 @@ type Product_variables struct {
 	Has_legacy_camera_hal1 struct {
 		Cflags []string
 	}
-
 	Uses_media_extensions struct {
 		Cflags []string
+	}
+	BoardUsesQTIHardware struct {
+		Cflags []string
+		Cppflags []string
+	}
+	BoardUsesQCOMHardware struct {
+		Cflags []string
+		Cppflags []string
+	}
+	TargetUsesQCOMBsp struct {
+		Cflags []string
+		Cppflags []string
+	}
+	TargetUsesQCOMLegacyBsp struct {
+		Cflags []string
+		Cppflags []string
+	}
+	BoardUsesLegacyAlsa struct {
+		Cflags []string
+		Cppflags []string
 	}
 }
 
@@ -32,4 +51,10 @@ type ProductVariables struct {
 	QTIAudioPath            *string `json:",omitempty"`
 	QTIDisplayPath          *string `json:",omitempty"`
 	QTIMediaPath            *string `json:",omitempty"`
+	Uses_non_treble_camera  *bool `json:",omitempty"`
+	BoardUsesQTIHardware  *bool `json:",omitempty"`
+	BoardUsesQCOMHardware  *bool `json:",omitempty"`
+	TargetUsesQCOMBsp  *bool `json:",omitempty"`
+	TargetUsesQCOMLegacyBsp  *bool `json:",omitempty"`
+	BoardUsesLegacyAlsa  *bool `json:",omitempty"`
 }
