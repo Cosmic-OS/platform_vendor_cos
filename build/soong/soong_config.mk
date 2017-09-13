226b9c56d70cdf6cf6ca9d534e073ac1d71a1294
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-13 19:06:33 -0400

    vendor/cos: Expose pathmaps to soong
    Change-Id: I3ba9575c96a116696d1820270e54202b24057a1b

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -8,7 +8,7 @@ cosmic_soong:
 	echo '    "Specific_camera_parameter_library": "$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)",'; \
 	echo '    "Target_shim_libs": "$(subst $(space),:,$(TARGET_LD_SHIM_LIBS))",'; \
 	echo '    "Uses_generic_camera_parameter_library": $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true),'; \
-	echo '    "Uses_qcom_bsp_legacy": $(if $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)),true,false)'; \
+	echo '    "Uses_qcom_bsp_legacy": $(if $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)),true,false),'; \
 	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
 	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \
 	echo '    "BoardUsesQTIHardware":  $(if $(BOARD_USES_QTI_HARDWARE),true,false),'; \
@@ -16,12 +16,23 @@ cosmic_soong:
 	echo '    "QTIAudioPath":  "$(call project-path-for,qcom-audio)",'; \
 	echo '    "QTIDisplayPath":  "$(call project-path-for,qcom-display)",'; \
 	echo '    "QTIMediaPath":  "$(call project-path-for,qcom-media)",';  \
+	echo '    "Uses_non_treble_camera": $(if $(filter true,$(TARGET_USES_NON_TREBLE_CAMERA)),true,false),'; \
+	echo '    "BTVendorPath": "$(call project-path-for,bt-vendor)",'; \
+	echo '    "RILPath": "$(call project-path-for,ril)",'; \
+	echo '    "WLANPath": "$(call project-path-for,wlan)"'; \
 	echo '},'; \
 	echo '"Qualcomm": {'; \
 	echo '    "BoardUsesQTIHardware": $(if $(filter true,$(BOARD_USES_QTI_HARDWARE)),true,false),'; \
 	echo '    "BoardUsesQCOMHardware": $(if $(filter true,$(BOARD_USES_QCOM_HARDWARE)),true,false),'; \
 	echo '    "TargetUsesQCOMBsp": $(if $(filter true,$(TARGET_USES_QCOM_BSP)),true,false),'; \
 	echo '    "TargetUsesQCOMLegacyBsp": $(if $(filter true,$(TARGET_USES_QCOM_LEGACY_BSP)),true,false),'; \
-	echo '    "BoardUsesLegacyAlsa": $(if $(filter true,$(BOARD_USES_LEGACY_ALSA_AUDIO)),true,false)'; \
+	echo '    "BoardUsesLegacyAlsa": $(if $(filter true,$(BOARD_USES_LEGACY_ALSA_AUDIO)),true,false),'; \
+	echo '    "QCOMAudioPath": "$(call project-path-for,qcom-audio)",'; \
+	echo '    "QCOMCameraPath": "$(call project-path-for,qcom-camera)",'; \
+	echo '    "QCOMDataservicesPath": "$(call project-path-for,qcom-dataservices)",';  \
+	echo '    "QCOMDisplayPath": "$(call project-path-for,qcom-display)",';  \
+	echo '    "QCOMGPSPath": "$(call project-path-for,qcom-gps)",';  \
+	echo '    "QCOMMediaPath": "$(call project-path-for,qcom-media)",';  \
+	echo '    "QCOMSensorsPath": "$(call project-path-for,qcom-sensors)"';  \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
