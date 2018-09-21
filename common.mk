31073d61020667884e764fafaf95486fbbd9038d
Sagar Shah <sagarshah2326@gmail.com>
2018-09-21 16:49:37 +0000

    vendor: Reuse the default base theme overlay
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- common.mk
+++ common.mk
@@ -22,7 +22,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.wfd.enable=1 \
     persist.sys.wfd.virtual=0 \
     ro.build.selinux=1 \
-#    ro.boot.vendor.overlay.theme=com.cosmic.overlay.base.stockfixed
+    ro.boot.vendor.overlay.theme=com.cosmic.overlay.base.stockfixed
 
 # LatinIME gesture typing
 ifneq ($(filter tenderloin,$(TARGET_PRODUCT)),)
