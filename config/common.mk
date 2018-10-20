63c10548fb37e40f55788ab37f4f237d50e059de
Kshitij Gupta <kshitijgm@gmail.com>
2018-10-20 15:49:42 +0530

    Introduce overlay for Lawnchair default conf
    Change-Id: Ie05eaea58ed341d0f4f2f5dcc177663afe401de1
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -153,7 +153,14 @@ PRODUCT_COPY_FILES += \
     vendor/cos/prebuilt/common/etc/permissions/privapp-permissions-lawnchair.xml:system/etc/permissions/privapp-permissions-lawnchair.xml \
     vendor/cos/prebuilt/common/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:system/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
 
-
+# Lawnchair Default Configuration
+PRODUCT_PACKAGES += \
+    Lawnchair \
+    LawnConf
+	
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.boot.vendor.overlay.theme=com.cos.overlay.lawnconf
+	
 # Storage manager
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.storage_manager.enabled=true
