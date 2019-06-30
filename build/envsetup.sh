4783bdcc935159b5252fd76b9fe6689fe02b7d67
Sagar Shah <sagarshah2326@gmail.com>
2019-06-30 17:01:45 +0200

    vendor: Cleanup envsetup.sh [SQUASH]
    Squashed commit of the following:
    
    commit 6f195839a861d61990fa22e790d6ccd63f0deb01
    Author: Sagar Shah <sagarshah2326@gmail.com>
    Date:   Sun Jun 30 16:57:53 2019 +0200
    
        vendor: Remove Pixel gerrit and remote functions
    
    commit 8d9f0b7a224bb45a77a9ab407a2cc3444b1936e7
    Author: Sagar Shah <sagarshah2326@gmail.com>
    Date:   Sun Jun 30 16:55:17 2019 +0200
    
        Revert "envsetup: Fix pixelremote for caf projects"
    
        This reverts commit a86342b3057d46832e4549ad84edde03d9586dba.
    
    commit 932b7092b6bdc40d265044e31519b37a9c0390c8
    Author: Sagar Shah <sagarshah2326@gmail.com>
    Date:   Sun Jun 30 16:55:16 2019 +0200
    
        Revert "vendor: envsetup: Fix git config remotes"
    
        This reverts commit 4f4fc3a6f85819da71a48aef0234fddb91c99008.
    
    commit 28a2cc66735c5f509cfaaa7ad341093a8334b729
    Author: Sagar Shah <sagarshah2326@gmail.com>
    Date:   Sun Jun 30 16:55:15 2019 +0200
    
        Revert "vendor: envsetup: Fix pixelremote again"
    
        This reverts commit ad0ab535914616d94b52ecde4ba39ddfcba2c54b.
    
    commit 06a27820f017296b5f0ce71295178db8f807f619
    Author: Sagar Shah <sagarshah2326@gmail.com>
    Date:   Sun Jun 30 16:55:14 2019 +0200
    
        Revert "vendor: Update repopick and envsetup"
    
        This reverts commit 110b16e73d5c9c9e096f93801bfc2e12caf352f0.
    
    commit 8cf377e98836979d0725499ea544d6eeecfba18d
    Author: Sagar Shah <sagarshah2326@gmail.com>
    Date:   Sun Jun 30 16:54:34 2019 +0200
    
        Revert "vendor: Fix 'pixelrebase' command"
    
        This reverts commit 235d6ca0b2fa314c1f33b5f06eade99590448f71.
    
    commit d937daedc92132ab6066d4525cfbfb0f6afcae91
    Author: Sagar Shah <sagarshah2326@gmail.com>
    Date:   Sun Jun 30 16:54:32 2019 +0200
    
        Revert "envsetup: Add githubremote function"
    
        This reverts commit 6d5f7dad90bf7a4389826240c0463e7dfaf09ba5.
    
    commit cfce80d2960f4df3f3bd9abf4da5ede07fb06189
    Author: Sagar Shah <sagarshah2326@gmail.com>
    Date:   Sun Jun 30 16:54:31 2019 +0200
    
        Revert "vendor: Add original lineage repopick"
    
        This reverts commit 153fed5d7118aec4eb9aaea70ffe8d5e094586c7.
    
    commit 8108f3dfbae3f18954c1f616e06a46154513e30e
    Author: Sagar Shah <sagarshah2326@gmail.com>
    Date:   Sun Jun 30 16:54:05 2019 +0200
    
        Revert "vendor: Fix 'githubremote' command"
    
        This reverts commit 1dc2651bfca61d7c4af8bf88ac6588427b213aff.
    
    commit 8c36ae308f8739f73a0185d3acb27542ee725cae
    Author: Sagar Shah <sagarshah2326@gmail.com>
    Date:   Sun Jun 30 16:54:04 2019 +0200
    
        Revert "vendor: pixelgerrit: Set default remote branch to push function"
    
        This reverts commit 1d1a4f92691524c112eba9103e9156a36e28e160.
    
    commit 0320a723986f4676140686808d547f197fd1194a
    Author: Sagar Shah <sagarshah2326@gmail.com>
    Date:   Sun Jun 30 16:54:02 2019 +0200
    
        Revert "vendor: pixelgerrit: Fix local branch argument on push function"
    
        This reverts commit b69024a9c0664f8464bcc15079f81648de8fbf27.
    
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -5,19 +5,14 @@ Additional functions:
 - mmp:             Builds all of the modules in the current directory and pushes them to the device.
 - mmap:            Builds all of the modules in the current directory and its dependencies, then pushes the package to the device.
 - mmmp:            Builds all of the modules in the supplied directories and pushes them to the device.
-- pixelgerrit:     A Git wrapper that fetches/pushes patch from/to PixelExperience Gerrit Review.
-- pixelrebase:     Rebase a Gerrit change and push it again.
 - aospremote:      Add git remote for matching AOSP repository.
 - cafremote:       Add git remote for matching CodeAurora repository.
-- githubremote:    Add git remote for PixelExperience Github.
 - mka:             Builds using SCHED_BATCH on all processors.
 - mkap:            Builds the module(s) using mka and pushes them to the device.
 - cmka:            Cleans and builds using mka.
 - repodiff:        Diff 2 different branches or tags within the same repo
 - repolastsync:    Prints date and time of last repo sync.
 - reposync:        Parallel repo sync using ionice and SCHED_BATCH.
-- repopick:        Utility to fetch changes from PixelExperience Gerrit.
-- losrepopick:     Utility to fetch changes from Lineage Gerrit.
 - installboot:     Installs a boot.img to the connected device.
 - installrecovery: Installs a recovery.img to the connected device.
 EOF
@@ -304,22 +299,6 @@ function cafremote()
     echo "Remote 'caf' created"
 }
 
-function githubremote()
-{
-    if ! git rev-parse --git-dir &> /dev/null
-    then
-        echo ".git directory not found. Please run this from the root directory of the Android repository you wish to set up."
-        return 1
-    fi
-    git remote rm github 2> /dev/null
-    local REMOTE=$(git config --get remote.pixel.projectname)
-
-    local PROJECT=$(echo $REMOTE | sed -e "s#platform/#android/#g; s#/#_#g")
-
-    git remote add github https://github.com/PixelExperience/$PROJECT
-    echo "Remote 'github' created"
-}
-
 function installboot()
 {
     if [ ! -e "$OUT/recovery/root/etc/recovery.fstab" ];
@@ -410,276 +389,26 @@ function installrecovery()
     fi
 }
 
-function pixelgerrit() {
-    if [ "$(__detect_shell)" = "zsh" ]; then
-        # zsh does not define FUNCNAME, derive from funcstack
-        local FUNCNAME=$funcstack[1]
-    fi
-
-    if [ $# -eq 0 ]; then
-        $FUNCNAME help
+function makerecipe() {
+    if [ -z "$1" ]
+    then
+        echo "No branch name provided."
         return 1
     fi
-    local user=`git config --get review.gerrit.pixelexperience.org.username`
-    local review=`git config --get remote.pixel.review`
-    local project=`git config --get remote.pixel.projectname`
-    local remote_branch=pie
-    local command=$1
-    shift
-    case $command in
-        help)
-            if [ $# -eq 0 ]; then
-                cat <<EOF
-Usage:
-    $FUNCNAME COMMAND [OPTIONS] [CHANGE-ID[/PATCH-SET]][{@|^|~|:}ARG] [-- ARGS]
+    cd android
+    sed -i s/'default revision=.*'/'default revision="refs\/heads\/'$1'"'/ default.xml
+    git commit -a -m "$1"
+    cd ..
 
-Commands:
-    fetch   Just fetch the change as FETCH_HEAD
-    help    Show this help, or for a specific command
-    pull    Pull a change into current branch
-    push    Push HEAD or a local branch to Gerrit
+    repo forall -c '
 
-Any other Git commands that support refname would work as:
-    git fetch URL CHANGE && git COMMAND OPTIONS FETCH_HEAD{@|^|~|:}ARG -- ARGS
-
-See '$FUNCNAME help COMMAND' for more information on a specific command.
-
-Example:
-    $FUNCNAME checkout -b topic 1234/5
-works as:
-    git fetch http://DOMAIN/p/PROJECT refs/changes/34/1234/5 \\
-      && git checkout -b topic FETCH_HEAD
-will checkout a new branch 'topic' base on patch-set 5 of change 1234.
-Patch-set 1 will be fetched if omitted.
-EOF
-                return
-            fi
-            case $1 in
-                __cmg_*) echo "For internal use only." ;;
-                changes|for)
-                    if [ "$FUNCNAME" = "pixelgerrit" ]; then
-                        echo "'$FUNCNAME $1' is deprecated."
-                    fi
-                    ;;
-                help) $FUNCNAME help ;;
-                fetch|pull) cat <<EOF
-usage: $FUNCNAME $1 [OPTIONS] CHANGE-ID[/PATCH-SET]
-
-works as:
-    git $1 OPTIONS http://DOMAIN/p/PROJECT \\
-      refs/changes/HASH/CHANGE-ID/{PATCH-SET|1}
-
-Example:
-    $FUNCNAME $1 1234
-will $1 patch-set 1 of change 1234
-EOF
-                    ;;
-                push) cat <<EOF
-usage: $FUNCNAME push [OPTIONS] [LOCAL_BRANCH]
-
-works as:
-    git push OPTIONS ssh://USER@DOMAIN:29418/PROJECT \\
-      {LOCAL_BRANCH|HEAD}:refs/for/$remote_branch
-
-Example:
-    $FUNCNAME push fix6789
-will push local branch 'fix6789' to Gerrit for branch '$remote_branch'.
-HEAD will be pushed from local if omitted.
-EOF
-                    ;;
-                *)
-                    $FUNCNAME __cmg_err_not_supported $1 && return
-                    cat <<EOF
-usage: $FUNCNAME $1 [OPTIONS] CHANGE-ID[/PATCH-SET][{@|^|~|:}ARG] [-- ARGS]
-
-works as:
-    git fetch http://DOMAIN/p/PROJECT \\
-      refs/changes/HASH/CHANGE-ID/{PATCH-SET|1} \\
-      && git $1 OPTIONS FETCH_HEAD{@|^|~|:}ARG -- ARGS
-EOF
-                    ;;
-            esac
-            ;;
-        __cmg_get_ref)
-            $FUNCNAME __cmg_err_no_arg $command $# && return 1
-            local change_id patchset_id hash
-            case $1 in
-                */*)
-                    change_id=${1%%/*}
-                    patchset_id=${1#*/}
-                    ;;
-                *)
-                    change_id=$1
-                    patchset_id=1
-                    ;;
-            esac
-            hash=$(($change_id % 100))
-            case $hash in
-                [0-9]) hash="0$hash" ;;
-            esac
-            echo "refs/changes/$hash/$change_id/$patchset_id"
-            ;;
-        fetch|pull)
-            $FUNCNAME __cmg_err_no_arg $command $# help && return 1
-            $FUNCNAME __cmg_err_not_repo && return 1
-            local change=$1
-            shift
-            git $command $@ http://$review/p/$project \
-                $($FUNCNAME __cmg_get_ref $change) || return 1
-            ;;
-        push)
-            $FUNCNAME __cmg_err_not_repo && return 1
-            if [ -z "$user" ]; then
-                echo >&2 "Gerrit username not found."
-                return 1
-            fi
-            local local_branch=${*: -1:1}
-            if [ -z "$local_branch" ]; then
-                local_branch=HEAD
-            fi
-            shift
-            git push $@ ssh://$user@$review:29418/$project \
-                $local_branch:refs/for/$remote_branch || return 1
-            ;;
-        changes|for)
-            if [ "$FUNCNAME" = "pixelgerrit" ]; then
-                echo >&2 "'$FUNCNAME $command' is deprecated."
-            fi
-            ;;
-        __cmg_err_no_arg)
-            if [ $# -lt 2 ]; then
-                echo >&2 "'$FUNCNAME $command' missing argument."
-            elif [ $2 -eq 0 ]; then
-                if [ -n "$3" ]; then
-                    $FUNCNAME help $1
-                else
-                    echo >&2 "'$FUNCNAME $1' missing argument."
-                fi
-            else
-                return 1
-            fi
-            ;;
-        __cmg_err_not_repo)
-            if [ -z "$review" -o -z "$project" ]; then
-                echo >&2 "Not currently in any reviewable repository."
-            else
-                return 1
-            fi
-            ;;
-        __cmg_err_not_supported)
-            $FUNCNAME __cmg_err_no_arg $command $# && return
-            case $1 in
-                #TODO: filter more git commands that don't use refname
-                init|add|rm|mv|status|clone|remote|bisect|config|stash)
-                    echo >&2 "'$FUNCNAME $1' is not supported."
-                    ;;
-                *) return 1 ;;
-            esac
-            ;;
-    #TODO: other special cases?
-        *)
-            $FUNCNAME __cmg_err_not_supported $command && return 1
-            $FUNCNAME __cmg_err_no_arg $command $# help && return 1
-            $FUNCNAME __cmg_err_not_repo && return 1
-            local args="$@"
-            local change pre_args refs_arg post_args
-            case "$args" in
-                *--\ *)
-                    pre_args=${args%%-- *}
-                    post_args="-- ${args#*-- }"
-                    ;;
-                *) pre_args="$args" ;;
-            esac
-            args=($pre_args)
-            pre_args=
-            if [ ${#args[@]} -gt 0 ]; then
-                change=${args[${#args[@]}-1]}
-            fi
-            if [ ${#args[@]} -gt 1 ]; then
-                pre_args=${args[0]}
-                for ((i=1; i<${#args[@]}-1; i++)); do
-                    pre_args="$pre_args ${args[$i]}"
-                done
-            fi
-            while ((1)); do
-                case $change in
-                    ""|--)
-                        $FUNCNAME help $command
-                        return 1
-                        ;;
-                    *@*)
-                        if [ -z "$refs_arg" ]; then
-                            refs_arg="@${change#*@}"
-                            change=${change%%@*}
-                        fi
-                        ;;
-                    *~*)
-                        if [ -z "$refs_arg" ]; then
-                            refs_arg="~${change#*~}"
-                            change=${change%%~*}
-                        fi
-                        ;;
-                    *^*)
-                        if [ -z "$refs_arg" ]; then
-                            refs_arg="^${change#*^}"
-                            change=${change%%^*}
-                        fi
-                        ;;
-                    *:*)
-                        if [ -z "$refs_arg" ]; then
-                            refs_arg=":${change#*:}"
-                            change=${change%%:*}
-                        fi
-                        ;;
-                    *) break ;;
-                esac
-            done
-            $FUNCNAME fetch $change \
-                && git $command $pre_args FETCH_HEAD$refs_arg $post_args \
-                || return 1
-            ;;
-    esac
-}
-
-function pixelrebase() {
-    local repo=$1
-    local refs=$2
-    local pwd="$(pwd)"
-    local dir="$(gettop)/$repo"
-
-    if [ -z $repo ] || [ -z $refs ]; then
-        echo "PixelExperience Gerrit Rebase Usage: "
-        echo "      pixelrebase <path to project> <patch IDs on Gerrit>"
-        echo "      The patch IDs appear on the Gerrit commands that are offered."
-        echo "      They consist on a series of numbers and slashes, after the text"
-        echo "      refs/changes. For example, the ID in the following command is 26/8126/2"
-        echo ""
-        echo "      git[...]ges_apps_Camera refs/changes/26/8126/2 && git cherry-pick FETCH_HEAD"
-        echo ""
-        return
+    if [ "$REPO_REMOTE" = "github" ]
+    then
+        pwd
+        pixelremote
+        git push pixel HEAD:refs/heads/'$1'
     fi
-
-    if [ ! -d $dir ]; then
-        echo "Directory $dir doesn't exist in tree."
-        return
-    fi
-    cd $dir
-    repo=$(git config --get remote.pixel.projectname)
-    echo "Starting branch..."
-    repo start tmprebase .
-    echo "Bringing it up to date..."
-    repo sync .
-    echo "Fetching change..."
-    git fetch "http://gerrit.pixelexperience.org/p/$repo" "refs/changes/$refs" && git cherry-pick FETCH_HEAD
-    if [ "$?" != "0" ]; then
-        echo "Error cherry-picking. Not uploading!"
-        return
-    fi
-    echo "Uploading..."
-    repo upload .
-    echo "Cleaning up..."
-    repo abandon tmprebase .
-    cd $pwd
+    '
 }
 
 function mka() {
@@ -883,16 +612,6 @@ alias mmap='dopush mma'
 alias mmmap='dopush mmma'
 alias mkap='dopush mka'
 alias cmkap='dopush cmka'
-
-function repopick() {
-    T=$(gettop)
-    $T/vendor/aosp/build/tools/repopick.py $@
-}
-
-function losrepopick() {
-    T=$(gettop)
-    $T/vendor/aosp/build/tools/losrepopick.py $@
-}
 
 function fixup_common_out_dir() {
     common_out_dir=$(get_build_var OUT_DIR)/target/common
