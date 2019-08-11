46bf2f48b09bf0802f06190bcb25f7c0e6b32495
Kshitij Gupta <kshitijgm@gmail.com>
2019-08-11 12:28:02 +0530

    vendor: cos: Refactor BoardConfigQcom to ProductConfigQcom
    - Multiple variables are now "readonly", since they need to be exported
      for other scripts early on. These variables shouldn't be set in
      BoardConfig* scripts anymore, and refactored + included in product
      configs. Due to this, various BOARD_* vars are now refactored to
      PRODUCT_*. This includes the following:
        BOARD_USES_QCOM_HARDWARE -> PRODUCT_USES_QCOM_HARDWARE
        TARGET_BOARD_PLATFORM -> PRODUCT_BOARD_PLATFORM
    - Note, TARGET_BOARD_PLATFORM is still being used in other places, but
      both vars have the same value.
    
    Change-Id: I42321e3ec466465350d06c7d46b240644a36efb6
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- config/BoardConfigCosmic.mk
+++ config/BoardConfigCosmic.mk
@@ -1,10 +1,3 @@
 include vendor/cos/config/BoardConfigKernel.mk
-
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-include vendor/cos/config/BoardConfigQcom.mk
-endif
-
 include vendor/cos/config/BoardConfigSoong.mk
-
-PRODUCT_SOONG_NAMESPACES += $(PATHMAP_SOONG_NAMESPACES)
 
