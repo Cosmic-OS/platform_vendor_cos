698717cd35bfbdb46e30e4f7f16d2673adb536e9
Sagar Shah <sagarshah2326@gmail.com>
2019-09-04 15:04:33 +0200

    vendor: cos: Deprecate vendorsetup
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- vendorsetup.sh
+++
@@ -1,4 +0,0 @@
-for combo in $(curl -s https://raw.githubusercontent.com/Cosmic-OS/platform_vendor_cos/corona-release/cos.devices  | sed -e 's/#.*$//' | awk '{printf "cos_%s-userdebug\n", $1}')
-do
-    add_lunch_combo $combo
-done
