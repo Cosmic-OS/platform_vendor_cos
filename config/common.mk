bd27d65405bbcb6628c190c95c4f9be039284922
Chris Crump <chris@cypheros.co>
2019-10-28 21:21:46 -0400

    vendor: cos: Ship Google Sans font family as overlay
    This adds support for custom fonts starting with Google Sans, making it an opt-in overlay
    
    Change-Id: Id281d447d9d581c408196abd16b8fed60b6682f8

--- config/common.mk
+++ config/common.mk
@@ -99,6 +99,11 @@ PRODUCT_COPY_FILES += \
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 
+# Fonts
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/cos/prebuilt/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
+	vendor/cos/prebuilt/etc/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+
 # Strip the local variable table and the local variable type table to reduce
 # the size of the system image. This has no bearing on stack traces, but will
 # leave less information available via JDWP.
