5dcd1097dc0bc113921f3dfbc3e81f5215a19644
Sagar Shah <sagarshah2326@gmail.com>
2019-11-11 15:18:03 +0100

    vendor: Adapt to Product uses qcom hardware instead of board
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -11,7 +11,7 @@ $(if $(USE_DEVICE_SPECIFIC_$(1)), \
 $(call project-set-path,qcom-$(2),$(strip $(path)))
 endef
 
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+ifeq ($(PRODUCT_USES_QCOM_HARDWARE),true)
 
 $(call set-device-specific-path,AUDIO,audio,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/audio)
 $(call set-device-specific-path,DISPLAY,display,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/display)
