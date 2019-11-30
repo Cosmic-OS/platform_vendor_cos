ReloadedOS/vendor_reloaded

build: Hack/fix for display HAL soong namespace
WaveEdits: [MEMEWORTHY HAX - TOPKEK]
* INFO: For some reason PRODUCT_SOONG_NAMESPACES doesn't get read by soong in CAF even
  though its exported by make. To workaround this, copy the display HAL to a temporary dir
  and then remove the soong_namespace{} from Android.bp to allow compilation.
* If the HAL doesn't have soong_namespace then it doesn't need this hack, so check for this first
* Removed an unnecessary if condition cause QCOM_HARDWARE_VARIANT gets set no matter what

Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

build/envsetup.sh
@@ -7,6 +7,8 @@ EOF
 
 # Make using all available CPUs
 function mka() {
+    source $ANDROID_BUILD_TOP/vendor/wave/build/disp_hals_setup.sh
+
     if [ -f $ANDROID_BUILD_TOP/$QTI_BUILDTOOLS_DIR/build/update-vendor-hal-makefiles.sh ]; then
         vendor_hal_script=$ANDROID_BUILD_TOP/$QTI_BUILDTOOLS_DIR/build/update-vendor-hal-makefiles.sh
         source $vendor_hal_script --check

Cosmic-OS/platform_vendor_cos

build/envsetup.sh
@@ -402,6 +402,7 @@ function makerecipe() {
 }
 
 function mka() {
+    source $ANDROID_BUILD_TOP/vendor/cos/build/disp_hals_setup.sh
     m -j "$@"
 }
 