1bfb267e83e5004fb30f1512913d3e205705f25d
Michael Bestas <mkbestas@gmail.com>
2021-11-13 03:14:47 +0200

    build: Enable mainline checking for GSI/emulator targets
    * This is originally done in build/make but it checks against aosp_* targets
    * Enable it for all of our lineage_* targets
    
    Change-Id: I9fa62c8f709bd649d114f24f241d42d471489ae5

--- build/target/product/cos_arm64.mk
+++ build/target/product/cos_arm64.mk
@@ -19,4 +19,7 @@ include vendor/cos/build/target/product/cos_generic_target.mk
 
 TARGET_NO_KERNEL_OVERRIDE := true
 
+# Enable mainline checking
+PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := relaxed
+
 PRODUCT_NAME := cos_arm64
