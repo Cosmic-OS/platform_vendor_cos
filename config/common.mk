awaken-lemonkebab/android_vendor_awaken

vendor: overlay: Update from SP1A.210812.016
Change-Id: Idd547f7c5d0a2cf4509d3254346f854f272232ea
Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

config/common.mk
@@ -73,10 +73,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=log
 
 # Gapps
-ifeq ($(USE_GAPPS),true)
-$(call inherit-product, vendor/gapps/gapps.mk)
-endif
-
+$(call inherit-product, vendor/awaken/config/gapps.mk)
 
 # Include AOSP audio files
 include vendor/awaken/config/aosp_audio.mk

config/gapps.mk
@@ -1,0 +1,14 @@
+# Gapps
+ifeq ($(USE_GAPPS),true)
+$(call inherit-product, vendor/gms/gms_full.mk)
+
+# Common Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/awaken/overlay-gapps/common
+
+# Exclude RRO Enforcement
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS +=  \
+    vendor/awaken/overlay-gapps
+
+$(call inherit-product, vendor/awaken/config/rro_overlays.mk)
+endif

Cosmic-OS/platform_vendor_cos

Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

config/common.mk
@@ -73,10 +73,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=log
 
 # Gapps
-ifeq ($(USE_GAPPS),true)
-$(call inherit-product, vendor/gapps/gapps.mk)
-endif
-
+$(call inherit-product, vendor/cos/config/gapps.mk)
 
 # Include AOSP audio files
 include vendor/cos/config/aosp_audio.mk

config/gapps.mk
@@ -1,0 +1,14 @@
+# Gapps
+ifeq ($(USE_GAPPS),true)
+$(call inherit-product, vendor/gms/gms_full.mk)
+
+# Common Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/cos/overlay-gapps/common
+
+# Exclude RRO Enforcement
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS +=  \
+    vendor/cos/overlay-gapps
+
+$(call inherit-product, vendor/cos/config/rro_overlays.mk)
+endif